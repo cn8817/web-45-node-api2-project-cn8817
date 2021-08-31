12963f513f2fc4d09fa4e8b276320e08c9609a69
cn8817 <christinenguyen1433@gmail.com>
2021-08-31 16:50:25 -0700

    all tests pass

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,15 +3,16 @@ const express = require('express')
 const Post = require('./posts-model')
 const router = express.Router()
 
-router.get('/', (req,res)=> {
-    Post.find(req.query)
-        .then(posts => {
-            res.status(200).json(posts)
-        })
-        .catch(err => {
-            res.status(500).json({message: 'The posts information could not be retrieved'})
-        })
-})
+router.get('/', (req, res) => {
+    Post.find()
+      .then(post => {
+        res.json(post);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: "The posts information could not be retrieved" });
+      });
+  });
 
 router.get('/:id', (req,res)=> {
     Post.findById(req.params.id)
@@ -95,18 +96,25 @@ router.put('/:id', (req,res) => {
     }
 })
 
-router.get('/:id/comments', (req,res) => {
+router.get('/:id/comments', (req,res)=> {
     Post.findCommentById(req.params.id)
-        .then(comment => {
-            if(!comment){
-                res.status(404).json({ message: "The post with the specified ID does not exist" })
+        .then(post => {
+            if(!post) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"})
             } else {
-                res.status(200).json(posts)
+                return Post.findPostComments(req.params.id)
+            }
+        })
+        .then(post => {
+            if (post) {
+                res.json(post)
             }
         })
         .catch(err => {
+            console.log(err)
             res.status(500).json({message: "The comments information could not be retrieved"})
         })
-})
+    })
 
 module.exports = router
