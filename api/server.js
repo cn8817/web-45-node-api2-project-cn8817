be38aae6e48e3d378e68bde6561bf15ee4ff2d0a
cn8817 <christinenguyen1433@gmail.com>
2021-08-31 10:58:39 -0700

    fixed server

--- api/server.js
+++ api/server.js
@@ -7,6 +7,6 @@ const server = express()
 
 server.use(express.json())
 
-server.use('/api/posts')
+server.use('/api/posts', postsRouter)
 
 module.exports = server
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require your server and launch it here
 const server = require('./api/server')
 
-server.listen(4000, () => {
-    console.log('server listening on http://localhost:4000')
+server.listen(5000, () => {
+    console.log('server listening on http://localhost:5000')
 })
