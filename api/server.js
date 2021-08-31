da1c4fa177aaf2de156b6849381f7c3b9b447448
cn8817 <christinenguyen1433@gmail.com>
2021-08-31 11:34:50 -0700

    test3

--- api/server.js
+++ api/server.js
@@ -1,18 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
-const express = require('express')
-const postsRouter = require('./posts/posts-router')
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
 
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
 server.use('/api/posts', postsRouter)
 
-server.get('/', (req, res) => {
-    res.send(`
-      <h2>test</h>
-    `);
-  });
-
-module.exports = server
+module.exports = server;
