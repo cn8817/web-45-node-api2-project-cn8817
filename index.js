9866ba68d21b3d2474d1977f49cdd9a99fa1749c
cn8817 <christinenguyen1433@gmail.com>
2021-08-31 11:12:40 -0700

    testing codegrade

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require your server and launch it here
-const server = require('./api/server')
+// const server = require('./api/server')
 
-server.listen(4000, () => {
-    console.log('server listening on localhost:4000')
-})
+// server.listen(4000, () => {
+//     console.log('server listening on localhost:4000')
+// })
